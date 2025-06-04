51b492ca3578f9cb9d5f2fc4b44188106d848902
SeoJeongYu <bbwest0709@gmail.com>
2025-06-04 13:17:39 +0900

    [FEAT] publicRestaurantApiJob 스케줄러 구현

--- batch-server/src/main/java/com/pickmylunch/batch/BatchServerApplication.java
+++ batch-server/src/main/java/com/pickmylunch/batch/BatchServerApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 @EntityScan(basePackages = {"com.pickmylunch.common.entity"})
