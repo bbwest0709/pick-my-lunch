814052b769d35c235bdca3b00042d937343c4204
SeoJeongYu <bbwest0709@gmail.com>
2025-06-12 12:32:46 +0900

    [ADD] 인증 예외 코드 추가

--- api-server/src/main/java/com/pickmylunch/api/global/exception/code/AuthExceptionCode.java
+++ api-server/src/main/java/com/pickmylunch/api/global/exception/code/AuthExceptionCode.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum AuthExceptionCode implements ExceptionCode {
     USER_NOT_FOUND(HttpStatus.UNAUTHORIZED, "사용자를 찾을 수 없습니다."),
-    ;
+    ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "Access Token 만료"),
+    INVALID_SIGNATURE_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "서명이 올바르지 않습니다."),
+    UNAUTHENTICATED(HttpStatus.UNAUTHORIZED, "접근 권한이 없습니다.");
 
     private final HttpStatus status;
     private final String message;
