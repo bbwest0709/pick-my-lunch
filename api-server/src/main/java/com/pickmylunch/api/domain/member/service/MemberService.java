31914b36b378d1fad24b77a96968b5f10b754009
SeoJeongYu <bbwest0709@gmail.com>
2025-06-15 02:05:55 +0900

    [FEAT] 회원가입 기능

--- api-server/src/main/java/com/pickmylunch/api/domain/member/service/MemberService.java
+++ api-server/src/main/java/com/pickmylunch/api/domain/member/service/MemberService.java
@@ -1,7 +1,9 @@
 package com.pickmylunch.api.domain.member.service;
 
 import com.pickmylunch.api.domain.member.MemberProperties;
+import com.pickmylunch.api.domain.member.dto.request.RegisterDto;
 import com.pickmylunch.api.domain.member.repository.MemberRepository;
+import com.pickmylunch.common.entity.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,6 +26,11 @@ public class MemberService {
 
     public boolean isEmailExist(String email) {
         return memberRepository.existsByEmail(email);
+    }
+
+    public void register(RegisterDto dto) {
+        Member member = dto.of(passwordEncoder.encode(dto.password()));
+        memberRepository.save(member);
     }
 
 }
