3a9450e22967aec646b6b6e51ad46f12dd4a7d9b
SeoJeongYu <bbwest0709@gmail.com>
2025-06-15 05:18:56 +0900

    [FEAT] CSV 헤더명 상수 클래스

---
+++ api-server/src/main/java/com/pickmylunch/api/domain/region/service/CSVHeaders.java
@@ -0,0 +1,10 @@
+package com.pickmylunch.api.domain.region.service;
+
+public final class CSVHeaders {
+    public static final String DOSI = "시도명";
+    public static final String SIGUNGU = "시군구명";
+    public static final String LON = "X";
+    public static final String LAT = "Y";
+
+    private CSVHeaders() {}
+}
