6c7ebcd8382188aa4cb12ee54080e005aad4aa3c
SeoJeongYu <bbwest0709@gmail.com>
2025-06-15 05:20:01 +0900

    [ADD] CSV 파일 파싱 에러 코드 추가

--- api-server/src/main/java/com/pickmylunch/api/global/exception/code/CommonExceptionCode.java
+++ api-server/src/main/java/com/pickmylunch/api/global/exception/code/CommonExceptionCode.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum CommonExceptionCode implements ExceptionCode {
     TRANS_ENTITY_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Entity 변환 에러"),
-    TRANS_JSON_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "JSON 변환 에러");
+    TRANS_JSON_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "JSON 변환 에러"),
+    CSV_PARSING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "CSV 파일 파싱 에러");
+    ;
 
     private final HttpStatus httpStatus;
     private final String message;
