d210e6028656a2504455f5ba275c2e6a317fe283
SeoJeongYu <bbwest0709@gmail.com>
2025-06-15 02:01:58 +0900

    [FEAT] 회원 ID(memberName) 중복 체크 API

---
+++ api-server/src/main/java/com/pickmylunch/api/domain/member/controller/MemberController.java
@@ -0,0 +1,21 @@
+package com.pickmylunch.api.domain.member.controller;
+
+import com.pickmylunch.api.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/exists/member-name")
+    public ResponseEntity<Boolean> checkDuplicateMemberName(@RequestParam String memberName) {
+        boolean exists = memberService.isMemberNameExist(memberName);
+        return ResponseEntity.ok(exists);
+    }
+
+}
