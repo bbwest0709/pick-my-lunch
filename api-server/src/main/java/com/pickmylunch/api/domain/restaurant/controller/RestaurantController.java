9b5b68f2a544a056e12f514d2859bd720c817644
SeoJeongYu <bbwest0709@gmail.com>
2025-06-18 20:23:05 +0900

    [FEAT] 맛집 상세 정보 조회 API

--- api-server/src/main/java/com/pickmylunch/api/domain/restaurant/controller/RestaurantController.java
+++ api-server/src/main/java/com/pickmylunch/api/domain/restaurant/controller/RestaurantController.java
@@ -1,13 +1,16 @@
 package com.pickmylunch.api.domain.restaurant.controller;
 
+import com.pickmylunch.api.domain.restaurant.dto.response.RestaurantDetailResponseDto;
 import com.pickmylunch.api.domain.restaurant.dto.response.RestaurantResponseDto;
 import com.pickmylunch.api.domain.restaurant.repository.RestaurantRepository;
 import com.pickmylunch.api.domain.restaurant.service.RestaurantService;
+import com.pickmylunch.common.entity.Restaurant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +26,10 @@ public class RestaurantController {
     @GetMapping
     public ResponseEntity<Page<RestaurantResponseDto>> getAllRestaurant(Pageable pageable) {
         return ResponseEntity.ok(restaurantService.getAllRestaurants(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RestaurantDetailResponseDto> getRestaurantById(@PathVariable String id) {
+        return ResponseEntity.ok(restaurantService.getRestaurantById(id));
     }
 }
