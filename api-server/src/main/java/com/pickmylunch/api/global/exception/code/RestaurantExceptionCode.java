b18f44339df0bb88fa751e46a788c1e7b77b2191
SeoJeongYu <bbwest0709@gmail.com>
2025-06-18 20:22:45 +0900

    [FEAT] 맛집 예외 코드 정의

---
+++ api-server/src/main/java/com/pickmylunch/api/global/exception/code/RestaurantExceptionCode.java
@@ -0,0 +1,25 @@
+package com.pickmylunch.api.global.exception.code;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum RestaurantExceptionCode implements ExceptionCode {
+    RESTAURANT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 맛집입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return null;
+    }
+
+    @Override
+    public String getMessage() {
+        return "";
+    }
+}
