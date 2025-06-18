844129871b1e432b41f36c18a2bcb523aae82ee8
SeoJeongYu <bbwest0709@gmail.com>
2025-06-18 20:21:59 +0900

    [FIX] ID 타입 String 수정

--- api-server/src/main/java/com/pickmylunch/api/domain/restaurant/repository/RestaurantRepository.java
+++ api-server/src/main/java/com/pickmylunch/api/domain/restaurant/repository/RestaurantRepository.java
@@ -3,5 +3,5 @@ package com.pickmylunch.api.domain.restaurant.repository;
 import com.pickmylunch.common.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long>, RestaurantRepositoryCustom {
+public interface RestaurantRepository extends JpaRepository<Restaurant, String>, RestaurantRepositoryCustom {
 }
