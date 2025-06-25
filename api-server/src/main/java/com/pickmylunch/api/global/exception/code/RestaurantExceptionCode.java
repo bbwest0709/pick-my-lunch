907745282f3987d8cd7988a8cf52a23c8b6b210f
SeoJeongYu <bbwest0709@gmail.com>
2025-06-25 21:56:49 +0900

    [ADD] 맛집 예외 코드 추가

--- api-server/src/main/java/com/pickmylunch/api/global/exception/code/RestaurantExceptionCode.java
+++ api-server/src/main/java/com/pickmylunch/api/global/exception/code/RestaurantExceptionCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum RestaurantExceptionCode implements ExceptionCode {
-    RESTAURANT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 맛집입니다.");
+    RESTAURANT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 맛집입니다."),
+    INVALID_SORT_TYPE(HttpStatus.BAD_REQUEST, "유효하지 않은 정렬 타입입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
