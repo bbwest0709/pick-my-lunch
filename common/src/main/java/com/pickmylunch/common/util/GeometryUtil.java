a82e5aef5896503345f79b0f6477c4d663cc7a7f
SeoJeongYu <bbwest0709@gmail.com>
2025-06-25 21:43:07 +0900

    [FIX] 좌표 변환 에러 해결
    - TM 좌표 → WGS84 변환
    - CoordinateTransform 객체 재사용으로 성능 최적화

--- common/src/main/java/com/pickmylunch/common/util/GeometryUtil.java
+++ common/src/main/java/com/pickmylunch/common/util/GeometryUtil.java
@@ -1,19 +1,43 @@
 package com.pickmylunch.common.util;
 
-import lombok.RequiredArgsConstructor;
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.GeometryFactory;
-import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.*;
+import org.locationtech.proj4j.*;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class GeometryUtil {
+
     private final GeometryFactory geometryFactory;
+    private final CoordinateTransform transform;
+
+    public GeometryUtil(GeometryFactory geometryFactory) {
+        this.geometryFactory = geometryFactory;
+
+        // 변환 객체 캐싱
+        CRSFactory crsFactory = new CRSFactory();
+        CoordinateReferenceSystem srcCrs = crsFactory.createFromName("EPSG:5179");
+        CoordinateReferenceSystem dstCrs = crsFactory.createFromName("EPSG:4326");
+        CoordinateTransformFactory transformFactory = new CoordinateTransformFactory();
+        this.transform = transformFactory.createTransform(srcCrs, dstCrs);
+    }
 
     public Point createPoint(final double lon, final double lat) {
         Point point = geometryFactory.createPoint(new Coordinate(lon, lat));
         point.setSRID(4326);
         return point;
+    }
+
+    public Point createPointFromTM(double x, double y) {
+        Coordinate coord = convertTmToWgs84(x, y);
+        Point point = geometryFactory.createPoint(coord);
+        point.setSRID(4326);
+        return point;
+    }
+
+    public Coordinate convertTmToWgs84(double x, double y) {
+        ProjCoordinate src = new ProjCoordinate(x, y);
+        ProjCoordinate dst = new ProjCoordinate();
+        transform.transform(src, dst);
+        return new Coordinate(dst.x, dst.y);
     }
 }
