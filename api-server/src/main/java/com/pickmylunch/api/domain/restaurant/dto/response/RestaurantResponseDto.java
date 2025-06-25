26e77189c126aa29aa3461551111015b22188de6
SeoJeongYu <bbwest0709@gmail.com>
2025-06-25 21:45:16 +0900

    [ADD] 지번주소(jibunAddress)와 도로명주소(doroAddress) 필드 추가

--- api-server/src/main/java/com/pickmylunch/api/domain/restaurant/dto/response/RestaurantResponseDto.java
+++ api-server/src/main/java/com/pickmylunch/api/domain/restaurant/dto/response/RestaurantResponseDto.java
@@ -7,18 +7,24 @@ public record RestaurantResponseDto(
         String id,
         String restaurantName,
         Category category,
+        String jibunAddress,
+        String doroAddress,
         double lon,
         double lat,
-        double ratingAverage) {
+        double ratingAverage,
+        String restaurantTel) {
 
     public static RestaurantResponseDto of(Restaurant restaurant) {
         return new RestaurantResponseDto(
                 restaurant.getId(),
                 restaurant.getRestaurantName(),
                 restaurant.getCategory(),
+                restaurant.getJibunDetailAddress(),
+                restaurant.getDoroDetailAddress(),
                 restaurant.getLocation().getX(),
                 restaurant.getLocation().getY(),
-                restaurant.getRatingAverage()
+                restaurant.getRatingAverage(),
+                restaurant.getRestaurantTel()
         );
     }
 }
