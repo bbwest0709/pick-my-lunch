881640456de404f34b1d86c8c8deb3cf5d6fb9ee
SeoJeongYu <bbwest0709@gmail.com>
2025-06-30 23:31:45 +0900

    [CHORE] dto 네이밍 수정

--- api-server/src/main/java/com/pickmylunch/api/domain/rating/dto/request/PostRatingRequestDto.java
+++ api-server/src/main/java/com/pickmylunch/api/domain/rating/dto/request/PostRatingRequestDto.java
@@ -5,23 +5,23 @@ import com.pickmylunch.common.entity.Restaurant;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
-public record PostRatingReqDto(
-    @NotNull
-    @Max(5)
-    BigDecimal score,
-    @NotBlank
-    String content
+public record PostRatingRequestDto(
+        @NotNull
+        @Max(5)
+        BigDecimal score,
+        @NotBlank
+        String content
 ) {
-
 
     public Rating toEntity(Long memberId, Restaurant restaurant) {
         return Rating.builder()
-            .score(score)
-            .memberId(memberId)
-            .restaurant(restaurant)
-            .content(content)
-            .build();
+                .score(score)
+                .memberId(memberId)
+                .restaurant(restaurant)
+                .content(content)
+                .build();
     }
 }
--- api-server/src/main/java/com/pickmylunch/api/domain/rating/dto/request/PutRatingRequestDto.java
+++ api-server/src/main/java/com/pickmylunch/api/domain/rating/dto/request/PutRatingRequestDto.java
@@ -3,19 +3,19 @@ package com.pickmylunch.api.domain.rating.dto.request;
 import com.pickmylunch.common.entity.Rating;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
-public record PutRatingReqDto(
-    @NotNull
-    BigDecimal score,
-    @NotBlank
-    String content
+public record PutRatingRequestDto(
+        @NotNull
+        BigDecimal score,
+        @NotBlank
+        String content
 ) {
-
-    public Rating toEntity(){
+    public Rating toEntity() {
         return Rating.builder()
-            .score(score)
-            .content(content)
-            .build();
+                .score(score)
+                .content(content)
+                .build();
     }
 }
