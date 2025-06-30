10824c9681ef0cdf12df5bb1c7cd8a00f1118b98
SeoJeongYu <bbwest0709@gmail.com>
2025-06-30 23:34:41 +0900

    [CHORE] url 수정

--- api-server/src/main/java/com/pickmylunch/api/domain/rating/controller/RatingController.java
+++ api-server/src/main/java/com/pickmylunch/api/domain/rating/controller/RatingController.java
@@ -1,52 +1,45 @@
 package com.pickmylunch.api.domain.rating.controller;
 
-import com.pickmylunch.api.domain.rating.dto.request.PostRatingReqDto;
-import com.pickmylunch.api.domain.rating.dto.request.PutRatingReqDto;
-import com.pickmylunch.api.domain.rating.service.RatingService;
+import com.pickmylunch.api.domain.rating.dto.request.*;
+import com.pickmylunch.api.domain.rating.service.*;
 import com.pickmylunch.api.global.security.details.AuthUser;
 import com.pickmylunch.api.global.util.UrlHelper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/restaurant/rates")
 public class RatingController {
 
     private final RatingService ratingService;
     private final String REDIRECT_URL = "/api/restaurant";
 
-    @PostMapping("/api/restaurant/rates/{restaurantId}")
+    @PostMapping("/{restaurantId}")
     public ResponseEntity<Long> postRating(
-        @PathVariable("restaurantId") String restaurantId,
-        @RequestBody PostRatingReqDto reqDto,
-        @AuthenticationPrincipal AuthUser authUser) {
-        Long result = ratingService.postRating(restaurantId, reqDto, authUser.getId());
+            @PathVariable("restaurantId") String restaurantId,
+            @RequestBody PostRatingRequestDto dto,
+            @AuthenticationPrincipal AuthUser authUser) {
+        Long result = ratingService.postRating(restaurantId, dto, authUser.getId());
         return ResponseEntity.created(UrlHelper.createUri(REDIRECT_URL, result)).body(result);
     }
 
-    @PutMapping("/api/restaurant/rates/{ratingId}")
+    @PutMapping("/{ratingId}")
     public ResponseEntity<Long> putRating(
-        @PathVariable("ratingId") Long ratingId,
-        @RequestBody PutRatingReqDto reqDto,
-        @AuthenticationPrincipal AuthUser authUser) {
-        Long result = ratingService.putRating(ratingId, reqDto, authUser.getId());
+            @PathVariable("ratingId") Long ratingId,
+            @RequestBody PutRatingRequestDto dto,
+            @AuthenticationPrincipal AuthUser authUser) {
+        Long result = ratingService.putRating(ratingId, dto, authUser.getId());
         return ResponseEntity.ok(result);
     }
 
-    @DeleteMapping("/api/restaurant/rates/{ratingId}")
+    @DeleteMapping("/{ratingId}")
     public ResponseEntity<Void> delRating(
-        @PathVariable("ratingId") Long ratingId,
-        @AuthenticationPrincipal AuthUser authUser) {
+            @PathVariable("ratingId") Long ratingId,
+            @AuthenticationPrincipal AuthUser authUser) {
         ratingService.delRating(ratingId, authUser.getId());
         return ResponseEntity.noContent().build();
     }
-
-
 }
--- api-server/src/main/java/com/pickmylunch/api/domain/rating/service/RatingService.java
+++ api-server/src/main/java/com/pickmylunch/api/domain/rating/service/RatingService.java
@@ -1,12 +1,11 @@
 package com.pickmylunch.api.domain.rating.service;
 
-import com.pickmylunch.api.domain.rating.dto.request.PostRatingReqDto;
-import com.pickmylunch.api.domain.rating.dto.request.PutRatingReqDto;
+import com.pickmylunch.api.domain.rating.dto.request.*;
 import com.pickmylunch.api.domain.rating.repository.RatingRepository;
+import com.pickmylunch.api.domain.restaurant.repository.RestaurantRepository;
 import com.pickmylunch.api.global.exception.BusinessLogicException;
-import com.pickmylunch.api.global.exception.code.RatingExceptionCode;
-import com.pickmylunch.common.entity.Rating;
-import com.pickmylunch.common.entity.Restaurant;
+import com.pickmylunch.api.global.exception.code.*;
+import com.pickmylunch.common.entity.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,8 +16,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class RatingService {
 
     private final RatingRepository ratingRepository;
+    private final RestaurantRepository restaurantRepository;
 
-    public Long postRating(String restaurantId, PostRatingReqDto reqDto, Long memberId) {
+    @Transactional(rollbackFor = Exception.class)
+    public Long postRating(String restaurantId, PostRatingRequestDto reqDto, Long memberId) {
         Restaurant savedRestaurant = validRestaurantExist(restaurantId);
         validAlreadyPost(restaurantId, memberId);
 
@@ -26,45 +27,44 @@ public class RatingService {
         return rating.getId();
     }
 
-    public Long putRating(Long ratingId, PutRatingReqDto reqDto, Long memberId) {
+    @Transactional(rollbackFor = Exception.class)
+    public Long putRating(Long ratingId, PutRatingRequestDto reqDto, Long memberId) {
         Rating rating = validRatingExist(ratingId);
         validMemberSame(rating, memberId);
         rating.put(reqDto.toEntity());
         return rating.getId();
     }
 
+    @Transactional(rollbackFor = Exception.class)
     public void delRating(Long ratingId, Long memberId) {
         ratingRepository.findById(ratingId)
-            .ifPresent(rating -> {
-                if(!rating.getMemberId().equals(memberId)) {
-                    throw new BusinessLogicException(RatingExceptionCode.MEMBER_NOT_SAME);
-                }
-                ratingRepository.delete(rating);
-            });
+                .ifPresent(rating -> {
+                    if (!rating.getMemberId().equals(memberId)) {
+                        throw new BusinessLogicException(RatingExceptionCode.MEMBER_NOT_SAME);
+                    }
+                    ratingRepository.delete(rating);
+                });
     }
 
-    /*
-    * VALID
-    * */
-    //TODO : Restaurant 관련 구조
     private Restaurant validRestaurantExist(String restaurantId) {
-        return null;
+        return restaurantRepository.findById(restaurantId)
+                .orElseThrow(() -> new BusinessLogicException(RestaurantExceptionCode.RESTAURANT_NOT_FOUND));
     }
 
     private void validAlreadyPost(String restaurantId, Long memberId) {
         ratingRepository.findByRestaurantIdAndMemberId(restaurantId, memberId)
-            .ifPresent(rating -> {
-                throw new BusinessLogicException(RatingExceptionCode.ALREADY_RATE);
-            });
+                .ifPresent(rating -> {
+                    throw new BusinessLogicException(RatingExceptionCode.ALREADY_RATE);
+                });
     }
 
     private Rating validRatingExist(Long ratingId) {
         return ratingRepository.findById(ratingId)
-            .orElseThrow(() -> new BusinessLogicException(RatingExceptionCode.ENTITY_NOT_FOUND));
+                .orElseThrow(() -> new BusinessLogicException(RatingExceptionCode.ENTITY_NOT_FOUND));
     }
 
     private void validMemberSame(Rating rating, Long memberId) {
-        if(!rating.getMemberId().equals(memberId)) {
+        if (!rating.getMemberId().equals(memberId)) {
             throw new BusinessLogicException(RatingExceptionCode.MEMBER_NOT_SAME);
         }
     }
