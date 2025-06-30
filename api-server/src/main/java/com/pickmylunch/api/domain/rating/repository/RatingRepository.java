f0d423057b3e3f82b3f9342bbad37998e0af49ce
SeoJeongYu <bbwest0709@gmail.com>
2025-06-30 23:32:32 +0900

    [REFACTOR] JPA 쿼리 메서드 변경

--- api-server/src/main/java/com/pickmylunch/api/domain/rating/repository/RatingRepository.java
+++ api-server/src/main/java/com/pickmylunch/api/domain/rating/repository/RatingRepository.java
@@ -1,21 +1,15 @@
 package com.pickmylunch.api.domain.rating.repository;
 
 import com.pickmylunch.common.entity.Rating;
+
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface RatingRepository extends JpaRepository<Rating, Long> {
 
-    @Query("""
-            select r from Rating r
-            where 
-              r.restaurant.id = :restaurantId
-            and
-              r.memberId = :memberId
-        """)
-    Optional<Rating> findByRestaurantIdAndMemberId(@Param("restaurantId") String restaurantId,
-        @Param("memberId") Long memberId);
+    Optional<Rating> findByRestaurantIdAndMemberId(String restaurantId, Long memberId);
 }
