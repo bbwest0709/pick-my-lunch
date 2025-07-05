4ad8094b9b7ca45aa805166352bdbe689efe93b7
SeoJeongYu <bbwest0709@gmail.com>
2025-07-05 22:13:52 +0900

    [ADD] 맛집 상세 정보 캐시용 키 추가
    - 기존 변수명 PATTERN, FORMAT 구분 명확하게 수정

--- api-server/src/main/java/com/pickmylunch/api/global/redis/RedisKeyGenerator.java
+++ api-server/src/main/java/com/pickmylunch/api/global/redis/RedisKeyGenerator.java
@@ -1,12 +1,14 @@
 package com.pickmylunch.api.global.redis;
 
 public class RedisKeyGenerator {
-    private static final String RESTAURANT_VIEW_KEY_PATTERN = "restaurant:%s:v";
+    private static final String RESTAURANT_VIEW_KEY_FORMAT = "restaurant:%s:v";
     private static final String RESTAURANT_VIEW_KEY_PATTERN_ALL = "restaurant:*:v";
-    private static final String MEMBER_LOCATION_KEY_PATTERN = "member_location:%d";
+    private static final String MEMBER_LOCATION_KEY_FORMAT = "member_location:%d";
+    private static final String RESTAURANT_DETAIL_KEY = "restaurant_details::%s";
+    private static final String RESTAURANT_DETAIL_KEY_PATTERN_ALL = "restaurant_details::*";
 
     public static String generateRestaurantViewKey(String restaurantId) {
-        return String.format(RESTAURANT_VIEW_KEY_PATTERN, restaurantId);
+        return String.format(RESTAURANT_VIEW_KEY_FORMAT, restaurantId);
     }
 
     public static String generateRestaurantViewKeyPattern() {
@@ -14,6 +16,14 @@ public class RedisKeyGenerator {
     }
 
     public static String generateRealTimeLocationKey(Long memberId) {
-        return String.format(MEMBER_LOCATION_KEY_PATTERN, memberId);
+        return String.format(MEMBER_LOCATION_KEY_FORMAT, memberId);
+    }
+
+    public static String generateRestaurantDetailKey(String restaurantId) {
+        return String.format(RESTAURANT_DETAIL_KEY, restaurantId);
+    }
+
+    public static String generateRestaurantDetailKeyPattern() {
+        return RESTAURANT_DETAIL_KEY_PATTERN_ALL;
     }
 }
