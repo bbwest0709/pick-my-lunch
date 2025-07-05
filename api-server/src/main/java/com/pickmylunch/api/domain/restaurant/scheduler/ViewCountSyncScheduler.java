f0ac323560cec0d1b7e73cbd614984c4a65aed88
SeoJeongYu <bbwest0709@gmail.com>
2025-07-05 22:45:47 +0900

    [ADD] 시간별 조회수 기반 캐시 갱신 로직 추가
    - 임계치 이상인 조회수를 가진 식당에 대해 캐시를 갱신하도록 기능 구현
    - 스케줄러 역할 명확화를 위해 네이밍 수정

--- api-server/src/main/java/com/pickmylunch/api/domain/restaurant/scheduler/ViewCountSyncScheduler.java
+++ api-server/src/main/java/com/pickmylunch/api/domain/restaurant/scheduler/ViewCountSyncScheduler.java
@@ -1,24 +1,27 @@
 package com.pickmylunch.api.domain.restaurant.scheduler;
 
-import com.pickmylunch.api.domain.restaurant.repository.RestaurantRepository;
+import com.pickmylunch.api.domain.restaurant.cache.ViewCountService;
+import com.pickmylunch.api.domain.restaurant.repository.*;
 import com.pickmylunch.api.global.redis.RedisRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
+import java.util.*;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class ViewCountSyncService {
+public class ViewCountSyncScheduler {
 
     private final RedisRepository redis;
     private final RestaurantRepository restaurantRepository;
+    private final ViewCountService viewCountService;
 
     @Scheduled(cron = "0 0 * * * *")
     public void syncAllViewCountsToDb() {
+        log.info("ViewCountSyncScheduler: Redis 조회수 DB 동기화 시작");
         Set<String> keys = redis.getAllRestaurantViewKeys();
         for (String key : keys) {
             String restaurantId = key.split(":")[1];
@@ -26,9 +29,11 @@ public class ViewCountSyncService {
 
             if (viewCount > 0) {
                 restaurantRepository.incrementTotalViewCount(restaurantId, viewCount);
-
+                viewCountService.refreshCacheByHourlyCount(restaurantId);
                 redis.resetViewCount(restaurantId);
+                log.info("ViewCountSyncScheduler: {} 조회수 {}만큼 DB 반영", restaurantId, viewCount);
             }
         }
+        log.info("ViewCountSyncScheduler: Redis 조회수 DB 동기화 종료");
     }
 }
