97389ee3d249a59bebd7e720bdc873ace1e4e32c
SeoJeongYu <bbwest0709@gmail.com>
2025-07-21 00:00:36 +0900

    [REFACTOR] RedisConfig 메서드 분리

--- api-server/src/main/java/com/pickmylunch/api/global/config/RedisConfig.java
+++ api-server/src/main/java/com/pickmylunch/api/global/config/RedisConfig.java
@@ -22,22 +22,7 @@ public class RedisConfig {
 
     @Bean
     public RedisCacheManager redisCacheManager(RedisConnectionFactory connectionFactory) {
-        ObjectMapper objectMapper = new ObjectMapper()
-                .registerModule(new JavaTimeModule())
-                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                .setSerializationInclusion(JsonInclude.Include.NON_NULL);
-
-        SimpleModule bigDecimalModule = new SimpleModule();
-        bigDecimalModule.addSerializer(BigDecimal.class, ToStringSerializer.instance);
-        objectMapper.registerModule(bigDecimalModule);
-
-        Jackson2JsonRedisSerializer<RestaurantDetailResponseDto> restaurantSerializer =
-                new Jackson2JsonRedisSerializer<>(RestaurantDetailResponseDto.class);
-        restaurantSerializer.setObjectMapper(objectMapper);
-
-        Jackson2JsonRedisSerializer<FindRatingResponseDto> ratingSerializer =
-                new Jackson2JsonRedisSerializer<>(FindRatingResponseDto.class);
-        ratingSerializer.setObjectMapper(objectMapper);
+        ObjectMapper objectMapper = objectMapper();
 
         RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
@@ -47,11 +32,37 @@ public class RedisConfig {
         RedisCacheManager redisCacheManager = RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(cacheConfig)
                 .withCacheConfiguration("restaurant_details", RedisCacheConfiguration.defaultCacheConfig()
-                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(restaurantSerializer)))
+                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(createRestaurantSerializer(objectMapper))))
                 .withCacheConfiguration("rating_details", RedisCacheConfiguration.defaultCacheConfig()
-                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(ratingSerializer)))
+                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(createRatingSerializer(objectMapper))))
                 .build();
 
         return redisCacheManager;
+    }
+
+    private ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        SimpleModule bigDecimalModule = new SimpleModule();
+        bigDecimalModule.addSerializer(BigDecimal.class, ToStringSerializer.instance);
+        objectMapper.registerModule(bigDecimalModule);
+        return objectMapper;
+    }
+
+    private Jackson2JsonRedisSerializer<FindRatingResponseDto> createRatingSerializer(ObjectMapper objectMapper) {
+        Jackson2JsonRedisSerializer<FindRatingResponseDto> ratingSerializer =
+                new Jackson2JsonRedisSerializer<>(FindRatingResponseDto.class);
+        ratingSerializer.setObjectMapper(objectMapper);
+        return ratingSerializer;
+    }
+
+    private Jackson2JsonRedisSerializer<RestaurantDetailResponseDto> createRestaurantSerializer(ObjectMapper objectMapper) {
+        Jackson2JsonRedisSerializer<RestaurantDetailResponseDto> restaurantSerializer =
+                new Jackson2JsonRedisSerializer<>(RestaurantDetailResponseDto.class);
+        restaurantSerializer.setObjectMapper(objectMapper);
+        return restaurantSerializer;
     }
 }
