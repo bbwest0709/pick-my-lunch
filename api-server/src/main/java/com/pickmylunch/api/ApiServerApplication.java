846ed38ac6bb140bf23f445495b45d1a96b18ddc
SeoJeongYu <bbwest0709@gmail.com>
2025-07-20 23:31:23 +0900

    [FIX] Caching 커밋 누락

--- api-server/src/main/java/com/pickmylunch/api/ApiServerApplication.java
+++ api-server/src/main/java/com/pickmylunch/api/ApiServerApplication.java
@@ -3,9 +3,11 @@ package com.pickmylunch.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableCaching
 @EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication(scanBasePackages = {
